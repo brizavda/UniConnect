8952ac2d1fdad8ea6b7462aed7a5080836c31616
MaFernandaRico <151652152+MaFernandaRico@users.noreply.github.com>
2024-05-02 22:28:33 -0600

    Update ControladorMensajeria.java

--- src/main/java/controller/ControladorMensajeria.java
+++ src/main/java/controller/ControladorMensajeria.java
@@ -1,4 +1,22 @@
 package controller;
 
+import view.IU_Principal;
+import model.Mensajeria.Mensaje;
+import model.Mensajeria.GestorMensajeria;
+
+
 public class ControladorMensajeria {
+    private GestorMensajeria gestorMensajeria;
+
+    public ControladorMensajeria() {
+        gestorMensajeria = new GestorMensajeria();
+    }
+
+    public void agregarMensaje(int idRemitente, String mensaje, int idDestinatario) {
+        gestorMensajeria.agregarMensaje(idRemitente, mensaje, idDestinatario);
+    }
+
+    public void verMensajes(int idUsuario) {
+        gestorMensajeria.verMensajes(idUsuario);
+    }
 }
