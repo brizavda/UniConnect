cb41b35152eebd283702cf3961a6cdd71e240dd0
MaFernandaRico <151652152+MaFernandaRico@users.noreply.github.com>
2024-05-02 22:30:56 -0600

    Update IU_Mensajeria.java

--- src/main/java/view/IU_Mensajeria.java
+++ src/main/java/view/IU_Mensajeria.java
@@ -1,38 +1,62 @@
 package view;
-
+import controller.ControladorMensajeria;
+import view.IU_Mensajeria;
 import java.util.Scanner;
 
 public class IU_Mensajeria {
-    Scanner scanner = new Scanner(System.in);
+     Scanner scanner = new Scanner(System.in);
+     ControladorMensajeria controladorMensajeria = new ControladorMensajeria();
     public void menu() {
+    boolean salir = false;
+
+        while (!salir) {
         System.out.println("=== Menú de Mensajería ===");
         System.out.println("1. Enviar Nuevo Mensaje");
         System.out.println("2. Ver Mensajes Nuevos");
         System.out.println("3. Salir");
         int opcion = scanner.nextInt();
-        //Agrega controlador
+       
+            scanner.nextLine(); 
+
+            switch (opcion) {
+                case 1:
+                    agregarMensaje();
+                    break;
+                case 2:
+                    verMensajes();
+                    break;
+                case 3:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intenta de nuevo.");
+            }
+        }
     }
 
     public void agregarMensaje() {
         System.out.println("=== Nuevo Mensaje ===");
-
         System.out.print("Remitente (ID): ");
         int idRemitente = scanner.nextInt();
-        scanner.nextLine(); // Consumir el salto de línea
+        scanner.nextLine(); 
 
         System.out.print("Mensaje: ");
         String mensaje = scanner.nextLine();
 
         System.out.print("Destinatario (ID): ");
         int idDestinatario = scanner.nextInt();
+        scanner.nextLine(); 
 
-        //Agrega controladorMensajeria con la logica
-
+        controladorMensajeria.agregarMensaje(idRemitente, mensaje, idDestinatario);
         System.out.println("Mensaje enviado con éxito.");
     }
 
-    public void verMensajes(){
+        //Agrega controladorMensajeria con la logica
+        
+        public void verMensajes() {
         System.out.println("=== Mensajes Nuevos ===");
-        //Agrega controlador y la logica de despliegue de los mensajes
+        int idUsuario = obtenerIdUsuario();
+        controladorMensajeria.verMensajes(idUsuario);
     }
+
 }
